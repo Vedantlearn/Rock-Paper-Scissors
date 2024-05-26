40316806ef8e149cd24f86a9d40f736d7e97994b
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-05-26 16:40:18 +0530

    Update RockPaperScissors.java

--- Rock Paper Scissor game/src/RockPaperScissors.java
+++ Rock Paper Scissor game/src/RockPaperScissors.java
@@ -1,5 +1,5 @@
 import java.util.Random;  
-import java.util.Scanner;
+import java.util.Scanner; 
 public class RockPaperScissors {
 	public static void main(String[] args) {  
 		Scanner scanner = new Scanner(System.in);
